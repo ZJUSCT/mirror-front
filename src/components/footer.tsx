ea026db2d7d52b179dba5dbb416968a6efa31671
RalXYZ <RalXYZ@pm.me>
2021-09-24 19:11:08 +0800

    fix: make footer looks better

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,26 +8,22 @@ export default () => (
         浙江大学开源镜像站是一个以普及开源软件，方便浙江省内的用户高效访问开源项目的各种资源的非盈利计划。
       </p>
       <p>
-        该镜像提供了包括 Arch Linux, CentOS, CPAN, CTAN, Cygwin, Debian, Docker, EPEL, ELPA, Fedora,
-        Gentoo, Hackage, Homebrew, Kali Linux, Linux Mint, Manjaro, NeuroDebian, openSUSE, OpenWRT,
-        PostgreSQL, Qubes, RFC, Stackage, Ubuntu 等项目源的镜像，以服务于教育和科学研究为目的，提倡自由、平等、协作、共享的精神。
+        本镜像站提供了包括 Docker, PostgreSQL, Ubuntu 等项目源的镜像，以服务于教育和科学研究为目的，提倡自由、平等、协作、共享的精神。
       </p>
     </div>
-    <div style={{width: "150px"}}>
-      <h3>
-        Contact us
-      </h3>
-      <p>GitHub</p>
-      <h3>
-        Special Thanks
-      </h3>
-      <p>
-        ZJU QSC
-      </p>
-      <p>
-        ZJU Info Center
-      </p>
+    <div>
+      <span className="footer-title">About Us</span>
+      <a className="link link-hover">E-mail</a>
+      <a className="link link-hover">GitHub</a>
+      <a className="link link-hover">Blog</a>
     </div>
+    <div>
+      <span className="footer-title">Special Thanks</span>
+      <a className="link link-hover">ZJU QSC</a>
+      <a className="link link-hover">ZJU Info Center</a>
+      <a className="link link-hover">THU TNUA</a>
+    </div>
+
     <div>
       <img src="https://www.zjusct.io/css/images/ZJUSCT_Header_Logo.png" style={{width: "200px", height:"auto"}}/>
     </div>
