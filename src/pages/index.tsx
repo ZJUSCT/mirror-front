4609e978997e2705c346f673acf9995b68adb578
NCJ <me@ncj.wiki>
2021-12-30 22:17:58 +0800

    remove search and count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,9 @@ export default () => {
   );
 
   useEffect(() => {
-    axios.get<Mirrorz>("/mirrors/")
+    axios.get<Mirrorz>("/api/mirrors/")
       .then(result => {
         setMirrors(result.data.mirrors);
-        rebuildIndex();
       })
       .catch(err => {
         setIsError(true);
@@ -29,71 +28,11 @@ export default () => {
       });
   }, []);
 
-  /**
-   * rebuilds the overall index based on the options
-   */
-  const rebuildIndex = () => {
-    const dataToSearch = new JsSearch.Search("cname");
-    /**
-     *  defines a indexing strategy for the data
-     * more about it in here https://github.com/bvaughn/js-search#configuring-the-index-strategy
-     */
-    dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy();
-    /**
-     * defines the sanitizer for the search
-     * to prevent some of the words from being excluded
-     *
-     */
-    dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer();
-    /**
-     * defines the search index
-     * read more in here https://github.com/bvaughn/js-search#configuring-the-search-index
-     */
-    dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex("cname");
-
-    dataToSearch.addIndex("cname"); // sets the index attribute for the data
-
-    dataToSearch.addDocuments(mirrors); // adds the data to be searched
-    setSearch(dataToSearch);
-    setIsLoading(false);
-  };
-
-  /**
-   * handles the input change and perform a search with js-search
-   * in which the results will be added to the state
-   */
-  const searchData = e => {
-    setSearchQuery(e.target.value);
-    setSearchResults(search.search(e.target.value) as Mirror[]);
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
   return (
     <div className="mx-8 my-2">
-      <div className="flex justify-center">
-        <div className="shadow stats bg-base-200 m-2 ">
-          <div className="stat bg-base-200">
-            <div className="stat-title">Number of Items</div>
-            <div className="stat-value">
-              {(searchQuery === "" ? mirrors : searchResults).length}
-            </div>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="Search" />
-              <input
-                id="Search"
-                value={searchQuery}
-                onChange={searchData}
-                placeholder="Enter the name of a mirror"
-                className="input input-primary input-bordered"
-              />
-            </form>
-          </div>
-        </div>
-      </div>
       <div>
         <SearchTable
-          queryResults={searchQuery === "" ? mirrors : searchResults}
+          queryResults={mirrors}
         />
       </div>
     </div>
