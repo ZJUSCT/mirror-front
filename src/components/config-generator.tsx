158f6a67ce1b58b2e1ca77303c9540dccfea176c
RalXYZ <RalXYZ@pm.me>
2022-01-28 23:47:50 +0800

    feat: migrate config-generator to mui

--- src/components/config-generator.tsx
+++ src/components/config-generator.tsx
@@ -1,19 +1,57 @@
 import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Grid,
+} from "@mui/material";
 
-export default ({ promptString, versionList, configGen }: { promptString: string, versionList: string[], configGen: (version: string) => string }) => {
-    const [version, setVersion] = useState(versionList[0])
-    return (
-        <div>{promptString}
-            <select id="selectBox" style={{ color: 'black' }} onChange={(event) => { setVersion(event.target.value) }}>
-                {
-                    versionList.map((item) => {
-                        return <option key={item}>{item}</option>
-                    })
-                }
-            </select>
-            <pre>
-                <code>{configGen(version)}</code>
-            </pre>
-        </div>
-    )
-}
+export default ({
+  promptString,
+  versionList,
+  configGen,
+}: {
+  promptString: string;
+  versionList: string[];
+  configGen: (version: string) => string;
+}) => {
+  const [version, setVersion] = useState(versionList[0]);
+  return (
+    <Box>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-end"
+      >
+        <Grid item sx={{ mb: 1 }}>
+          {promptString}
+        </Grid>
+        <Grid item>
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-helper-label">
+              Version
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              label="Age"
+              onChange={event => {
+                setVersion(event.target.value as string);
+              }}
+            >
+              {versionList.map(item => {
+                return <MenuItem value={item}>{item}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <pre>
+        <code>{configGen(version)}</code>
+      </pre>
+    </Box>
+  );
+};
