2585a5a60971add265f329dcd4a3736f8aa964f3
Howyoung <howyoung3@gmail.com>
2022-03-14 20:55:18 +0800

    feat: API call error check

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,18 @@
 import config from '../../config';
 import { MirrorDto } from '../types/mirror';
 
-export async function getMirrors(): Promise<MirrorDto[]> {
-    return await (await fetch(config.apiBaseUrl + "/mirrors")).json();
+async function apiCall<T>(path: string): Promise<T> {
+  const res = await fetch(`${config.apiBaseUrl}/${path}`);
+  if (!res.ok) {
+    throw new Error(`API call failed: ${res.status} ${await res.text()}`);
+  }
+  return await res.json();
 }
 
-export async function getMirror(id: string): Promise<MirrorDto> {
-    return await (await fetch(config.apiBaseUrl + `/mirrors/${id}`)).json();
+export function getMirrors(): Promise<MirrorDto[]> {
+  return apiCall('mirrors');
+}
+
+export function getMirror(id: string): Promise<MirrorDto> {
+  return apiCall(`mirrors/${id}`);
 }
