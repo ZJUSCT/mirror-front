ae842ef9b0db8998ce99cd84377f197b810428d6
Azuk <me@azuk.top>
2022-06-15 23:36:52 +0800

    fix doc links

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,10 +1,10 @@
 import config from '../../config';
 
-export function getUrl(url: string | undefined): string {
+export function getUrl(url: string | undefined, prefix: boolean): string {
     if (!url) return config.siteUrl;
     if (process.env.NODE_ENV === 'development') {
         return url;
     } else {
-        return `${config.siteUrl}${url}`;
+        return `${config.siteUrl}${prefix ? config.pathPrefix : ''}${url}`;
     }
 }
