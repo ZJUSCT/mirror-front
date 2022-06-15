851c8519d9df5ac65781573a53d7aeb7256698aa
Azuk <me@azuk.top>
2022-06-15 23:37:12 +0800

    use google fonts reverse proxy

---
+++ cached-restore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if cmp -s "yarn.lock" "node_modules/yarn.lock"; then
+  echo "yarn.lock did not change, skipping install"
+else
+  echo "yarn.lock changed, reinstalling packages"
+  yarn config set registry https://registry.npmmirror.com
+  npm config set sharp_binary_host https://npmmirror.com/mirrors/sharp
+  npm config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips
+  yarn install --frozen-lockfile
+  sed -i 's/fonts.googleapis.com/fonts.loli.net/g' node_modules/gatsby-plugin-webfonts/modules/google.js
+  cp yarn.lock node_modules/yarn.lock
+fi
