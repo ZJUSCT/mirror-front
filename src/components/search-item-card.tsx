1101f83e1c0bb2b449c978d9585ea7f6bfb85523
Azuk <me@azuk.top>
2022-06-15 19:23:40 +0800

    add help button for items with docs

--- src/components/search-item-card.tsx
+++ src/components/search-item-card.tsx
@@ -1,6 +1,8 @@
 import { Card, Grid, Typography } from "@mui/material";
 import { useI18next } from "gatsby-plugin-react-i18next";
 import { CardActionArea } from "gatsby-theme-material-ui";
+import IconButton from "@mui/material/IconButton";
+import HelpIcon from '@mui/icons-material/Help';
 import * as React from "react";
 import { Mirror } from "../types/mirror";
 import StatusIndicator from "./status-indicator";
@@ -10,7 +12,12 @@ export default (props: { queryItem: Mirror }) => {
   const { language } = useI18next();
 
   return (
-    <Card className="zju-mirror-card" style={{ height: "100%" }}>
+    <Card className="zju-mirror-card" style={{ height: "100%", position: "relative" }}>
+      {props.queryItem.docUrl &&
+        <IconButton aria-label="help" href={getUrl(props.queryItem.docUrl)} style={{ position: "absolute", right: 0, zIndex: 1 }}>
+          <HelpIcon />
+        </IconButton>
+      }
       <CardActionArea
         to={getUrl(props.queryItem.url || props.queryItem.docUrl)}
         style={{ height: "100%" }}
