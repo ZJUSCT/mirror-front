fad9af097c6ed64586ab9535835db44758c450c0
Azuk <me@azuk.top>
2022-06-16 00:33:03 +0800

    disable cache in ssr

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,8 +1,10 @@
 function writeCache(key: string, data: string) {
+    if (typeof localStorage === `undefined`) return;
     localStorage.setItem(`__cache_${key}`, JSON.stringify(data));
 }
 
 function readCache(key: string, defaultValue: any): any {
+    if (typeof localStorage === `undefined`) return defaultValue;
     const d = localStorage.getItem(`__cache_${key}`);
     return d ? JSON.parse(d) : defaultValue;
 }
