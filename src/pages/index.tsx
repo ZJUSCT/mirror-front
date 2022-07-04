48d77be72d4f9f86fa7a0bebbbf76f44191f0e03
Azuk <me@azuk.top>
2022-07-04 23:53:51 +0800

    add network indicator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,45 @@
-import { Box, Grid, Typography } from "@mui/material";
-import { graphql } from "gatsby";
-import { Trans, useI18next } from "gatsby-plugin-react-i18next";
-import React, { useEffect, useState } from "react";
-import Footer from "../components/footer";
-import FrequentlyUsedMirrorCard from "../components/frequently-used-mirror-card";
-import LanguageIconButton from "../components/language-icon-button";
-import SearchTable from "../components/search-table";
-import Seo from "../components/seo";
-import ThemeIconButton from "../components/theme-icon-button";
-import { Mirror, MirrorDto } from "../types/mirror";
-import frequentlyUsedMirror from "../utils/frequently-used-mirror-list";
-import { getUrl } from "../utils/url";
-import { readCache, writeCache } from "../utils/cache";
-import NameIconButton from "../components/name-icon-button";
+import { Box, Chip, Grid, Typography } from '@mui/material';
+import { graphql } from 'gatsby';
+import { Trans, useI18next } from 'gatsby-plugin-react-i18next';
+import React, { useEffect, useState } from 'react';
+import Footer from '../components/footer';
+import FrequentlyUsedMirrorCard from '../components/frequently-used-mirror-card';
+import LanguageIconButton from '../components/language-icon-button';
+import SearchTable from '../components/search-table';
+import Seo from '../components/seo';
+import ThemeIconButton from '../components/theme-icon-button';
+import { Mirror, MirrorDto } from '../types/mirror';
+import frequentlyUsedMirror from '../utils/frequently-used-mirror-list';
+import { getUrl } from '../utils/url';
+import { readCache, writeCache } from '../utils/cache';
+import NameIconButton from '../components/name-icon-button';
 
 interface Data {
   mirrorDocs: {
     nodes: {
-      slug: string,
-      locale: string,
+      slug: string;
+      locale: string;
       frontmatter?: {
-        mirrorId?: string,
-      }
-    }[]
-  }
+        mirrorId?: string;
+      };
+    }[];
+  };
 }
+
+const networkMap = {
+  0: {
+    text: '校外网络',
+    color: 'info',
+  },
+  1: {
+    text: '校内网络 - IPv4',
+    color: 'primary',
+  },
+  2: {
+    text: '校内网络 - IPv6',
+    color: 'success',
+  },
+};
 
 async function fetchMirrors(): Promise<MirrorDto[]> {
   const res = await fetch('/api/mirrors');
@@ -36,23 +51,37 @@ async function fetchMirrors(): Promise<MirrorDto[]> {
   return json;
 }
 
-export default ({ data }: { data: Data }) => {
+async function fetchNetworkMode(): Promise<number> {
+  const res = await fetch('/api/is_campus_network');
+  if (!res.ok) {
+    return 0;
+  }
+  const json = await res.json();
+  return Number.isInteger(json) ? json : 0;
+}
+
+const Index =  ({ data }: { data: Data }) => {
   const { language, t } = useI18next();
 
+  const [networkMode, setNetworkMode] = useState<number>(0);
   const [mirrorsRaw, setMirrorsRaw] = useState<MirrorDto[]>(readCache('mirrors', []));
 
   useEffect(() => {
     fetchMirrors()
       .then(d => setMirrorsRaw(d))
       .catch(err => console.error(err));
+    fetchNetworkMode()
+      .then(d => setNetworkMode(d))
+      .catch(err => console.error(err));
   }, []);
 
-  const mirrorDocUrls = React.useMemo<Record<string, string>>(() =>
-    Object.fromEntries(
-      data.mirrorDocs.nodes
-        .filter(d => d.frontmatter?.mirrorId && d.locale === language)
-        .map(d => [d.frontmatter!.mirrorId, d.slug])
-    ),
+  const mirrorDocUrls = React.useMemo<Record<string, string>>(
+    () =>
+      Object.fromEntries(
+        data.mirrorDocs.nodes
+          .filter(d => d.frontmatter?.mirrorId && d.locale === language)
+          .map(d => [d.frontmatter!.mirrorId, d.slug])
+      ),
     [data, language]
   );
 
@@ -67,20 +96,37 @@ export default ({ data }: { data: Data }) => {
     [mirrorsRaw, mirrorDocUrls]
   );
 
+
   return (
-    <Box sx={{
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-    }}>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
       <Seo title="ZJU Mirror" />
-      <Grid container spacing={{ xs: 6 }} columns={{ xs: 1 }} sx={{ px: { xs: 4, sm: 8 }, py: 8 }}>
+      <Grid
+        container
+        spacing={{ xs: 6 }}
+        columns={{ xs: 1 }}
+        sx={{ px: { xs: 4, sm: 8 }, py: 8 }}
+      >
         <Grid item xs={1}>
-          <Grid container direction="row" justifyContent="space-between" alignItems="center">
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
             <Grid item>
               <Typography variant="h3" component="div" color="primary">
                 <Trans>ZJU Mirror</Trans>
+                <Chip
+                  label={t(networkMap[networkMode].text)}
+                  color={networkMap[networkMode].color}
+                />
               </Typography>
             </Grid>
             <Grid item>
@@ -98,10 +144,10 @@ export default ({ data }: { data: Data }) => {
             <Trans>常用镜像</Trans>
           </Typography>
           <Grid container spacing={{ xs: 2 }} columns={{ xs: 1, sm: 3, md: 6 }}>
-            {
-              frequentlyUsedMirror.map((e, i) => {
-                const mirror = mirrors[e.id];
-                return mirror && (
+            {frequentlyUsedMirror.map((e, i) => {
+              const mirror = mirrors[e.id];
+              return (
+                mirror && (
                   <Grid item xs={1} key={i}>
                     <FrequentlyUsedMirrorCard
                       name={mirror.name[language]}
@@ -111,8 +157,8 @@ export default ({ data }: { data: Data }) => {
                     />
                   </Grid>
                 )
-              })
-            }
+              );
+            })}
           </Grid>
         </Grid>
         <Grid item xs={1}>
@@ -147,3 +193,5 @@ export const query = graphql`
     }
   }
 `;
+
+export default Index;
