75fa1dc85c17622bd4c477ee5a863907313ed40c
NCJ <me@ncj.wiki>
2023-03-01 21:22:48 +0800

    deploy: use yarn config instead

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM node:17-bullseye
 WORKDIR /app
 COPY package.json yarn.lock ./
 RUN yarn config set registry 'https://registry.npmmirror.com/' && \ 
-    npm config set sharp_binary_host https://npmmirror.com/mirrors/sharp && \ 
-    npm config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips && \
+    yarn config set sharp_binary_host https://npmmirror.com/mirrors/sharp && \ 
+    yarn config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips && \
     yarn install --frozen-lockfile
 
 COPY . .
--- cached-restore.sh
+++ cached-restore.sh
@@ -8,8 +8,8 @@ if cmp -s "yarn.lock" "node_modules/yarn.lock"; then
 else
   echo "yarn.lock changed, reinstalling packages"
   yarn config set registry https://registry.npmmirror.com
-  npm config set sharp_binary_host https://npmmirror.com/mirrors/sharp
-  npm config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips
+  yarn config set sharp_binary_host https://npmmirror.com/mirrors/sharp
+  yarn config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips
   yarn install --frozen-lockfile
   sed -i 's/fonts.googleapis.com/fonts.loli.net/g' node_modules/gatsby-plugin-webfonts/modules/google.js
   cp yarn.lock node_modules/yarn.lock
