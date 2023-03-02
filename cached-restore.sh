42d4a544e4635d17abf9ae472e31cea4a11c1df0
NCJ <me@ncj.wiki>
2023-03-02 12:16:13 +0800

    chore: use zju mirror npm

--- cached-restore.sh
+++ cached-restore.sh
@@ -7,7 +7,7 @@ if cmp -s "yarn.lock" "node_modules/yarn.lock"; then
   echo "yarn.lock did not change, skipping install"
 else
   echo "yarn.lock changed, reinstalling packages"
-  yarn config set registry https://registry.npmmirror.com
+  yarn config set registry https://mirrors.zju.edu.cn/npm
   yarn config set sharp_binary_host https://npmmirror.com/mirrors/sharp
   yarn config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips
   yarn install --frozen-lockfile
