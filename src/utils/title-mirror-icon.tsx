e1fccc31972cbe34282f9971bcda8d28a46d26b1
RalXYZ <RalXYZ@pm.me>
2023-03-19 00:58:41 +0800

    feat: add more icons

--- src/utils/title-mirror-icon.tsx
+++ src/utils/title-mirror-icon.tsx
@@ -5,6 +5,7 @@ import alpineLinuxIcon from '@iconify/icons-simple-icons/alpinelinux';
 import anacondaIcon from '@iconify/icons-simple-icons/anaconda';
 import archLinuxIcon from '@iconify/icons-simple-icons/archlinux';
 import centOsIcon from '@iconify/icons-simple-icons/centos';
+import cpanIcon from '@iconify/icons-file-icons/cpan';
 import debianIcon from '@iconify/icons-simple-icons/debian';
 import deepinIcon from '@iconify/icons-simple-icons/deepin';
 import dockerIcon from '@iconify/icons-simple-icons/docker';
@@ -19,8 +20,11 @@ import npmIcon from '@iconify/icons-simple-icons/npm';
 import openWrtIcon from '@iconify/icons-simple-icons/openwrt';
 import pythonIcon from '@iconify/icons-simple-icons/python';
 import raspberryPiIcon from '@iconify/icons-simple-icons/raspberrypi';
+import rLanguageIcon from '@iconify/icons-mdi/language-r';
 import rockyIcon from '@iconify/icons-simple-icons/rockylinux';
 import rosIcon from '@iconify/icons-simple-icons/ros';
+import rustIcon from '@iconify/icons-simple-icons/rust';
+import texIcon from '@iconify/icons-file-icons/latex';
 import ubuntuIcon from '@iconify/icons-simple-icons/ubuntu';
 
 export default function TitleMirrorIcon(
@@ -29,16 +33,26 @@ export default function TitleMirrorIcon(
   size: string
 ) {
   switch (mirrorName) {
+    case 'CPAN':
+      return <Icon height={size} icon={cpanIcon} color={color} />;
+    case 'CRAN':
+      return <Icon height={size} icon={rLanguageIcon} color={color} />;
+    case 'CTAN':
+      return <Icon height={size} icon={texIcon} color={color} />;
     case 'alpine':
       return <Icon height={size} icon={alpineLinuxIcon} color={color} />;
     case 'anaconda':
       return <Icon height={size} icon={anacondaIcon} color={color} />;
     case 'archlinux':
       return <Icon height={size} icon={archLinuxIcon} color={color} />;
+    case 'archlinuxcn':
+      return <Icon height={size} icon={archLinuxIcon} color={color} />;
     case 'centos':
       return <Icon height={size} icon={centOsIcon} color={color} />;
     case 'centos-vault':
       return <Icon height={size} icon={centOsIcon} color={color} />;
+    case 'crates.io-index.git':
+      return <Icon height={size} icon={rustIcon} color={color} />;
     case 'debian':
       return <Icon height={size} icon={debianIcon} color={color} />;
     case 'deepin':
@@ -70,6 +84,8 @@ export default function TitleMirrorIcon(
     case 'pypi':
       return <Icon height={size} icon={pythonIcon} color={color} />;
     case 'raspberrypi':
+      return <Icon height={size} icon={raspberryPiIcon} color={color} />;
+    case 'raspbian':
       return <Icon height={size} icon={raspberryPiIcon} color={color} />;
     case 'rocky':
       return <Icon height={size} icon={rockyIcon} color={color} />;
