f7ea332400c537e93b9fd4dfb87451b9c8c93677
RalXYZ <RalXYZ@pm.me>
2023-03-25 02:52:58 +0800

    feat: add certificated icon

--- src/components/search-item-card.tsx
+++ src/components/search-item-card.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Typography } from '@mui/material';
+import { Card, Grid, Typography, Tooltip } from '@mui/material';
 import { useI18next } from 'gatsby-plugin-react-i18next';
 import { CardActionArea } from 'gatsby-theme-material-ui';
 import * as React from 'react';
@@ -6,6 +6,8 @@ import { Locale, Mirror } from '../types/mirror';
 import StatusIndicator from './status-indicator';
 import { getUrl } from '../utils/url';
 import { usePrefs } from './preferences-context';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import officialCertificatedMirrorList from '../utils/official-certificated-mirror-list';
 
 const SearchItemCard = (props: { queryItem: Mirror }) => {
   const { language } = useI18next();
@@ -29,12 +31,42 @@ const SearchItemCard = (props: { queryItem: Mirror }) => {
           height="100%"
           padding={2}
         >
-          <Grid item>
-            <Typography variant="h6" component="div">
-              {prefs.friendlyName
-                ? props.queryItem.name[lang]
-                : props.queryItem.id}
-            </Typography>
+          <Grid item width="100%">
+            <Grid
+              container
+              direction="row"
+              flexWrap="nowrap"
+              justifyContent="flex-start"
+              alignItems="flex-end"
+              spacing={1}
+            >
+              <Grid item>
+                <Typography variant="h6" component="div">
+                  {prefs.friendlyName
+                    ? props.queryItem.name[lang]
+                    : props.queryItem.id}
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                display={
+                  officialCertificatedMirrorList.includes(props.queryItem.id)
+                    ? 'block'
+                    : 'none'
+                }
+              >
+                <Tooltip
+                  title={
+                    lang === 'zh'
+                      ? `已加入 ${props.queryItem.id} 官方镜像列表`
+                      : `Added to ${props.queryItem.id} official mirror list`
+                  }
+                  placement="top"
+                >
+                  <VerifiedIcon fontSize="small" color="primary" />
+                </Tooltip>
+              </Grid>
+            </Grid>
             <Typography gutterBottom variant="body2" color="text.secondary">
               {props.queryItem.desc[lang]}
             </Typography>
---
+++ src/utils/official-certificated-mirror-list.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default [
+  "CRAN",
+  "CTAN",
+  "alpine",
+  "archlinux",
+  "archlinuxcn",
+  "centos",
+  "cygwin",
+  "deepin",
+  "deepin-cd",
+  "EPEL",
+  "fedora",
+  "openeuler",
+  "raspbian",
+  "rocky",
+  "ros",
+  "ubuntu",
+  "ubuntu-releases",
+]
