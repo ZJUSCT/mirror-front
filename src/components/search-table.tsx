eedd817bfc01e930352392bb8881fef1691a8da8
RalXYZ <RalXYZ@pm.me>
2023-05-13 01:03:46 +0800

    feat: sort front page mirror cards

--- src/components/search-table.tsx
+++ src/components/search-table.tsx
@@ -11,11 +11,16 @@ export default (props: { queryResults: Mirror[] }) => (
       columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}
       alignItems="stretch"
     >
-      {props.queryResults?.map((item, i) => (
-        <Grid item xs={1} key={i}>
-          <SearchItemCard queryItem={item} />
-        </Grid>
-      ))}
+      {
+        // sort by queryResults.id
+        props.queryResults
+          ?.sort((a, b) => a.id.toLowerCase().localeCompare(b.id.toLowerCase()))
+          .map((item, i) => (
+            <Grid item xs={1} key={i}>
+              <SearchItemCard queryItem={item} />
+            </Grid>
+          ))
+      }
     </Grid>
   </Box>
 );
