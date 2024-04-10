c8f363ffc9b7bdad5422659565a2185a431199f2
Azuk 443 <zhly@zju.edu.cn>
2024-04-10 12:03:28 +0000

    Update npm mirror url

--- cached-restore.sh
+++ cached-restore.sh
@@ -7,9 +7,9 @@ if cmp -s "yarn.lock" "node_modules/yarn.lock"; then
   echo "yarn.lock did not change, skipping install"
 else
   echo "yarn.lock changed, reinstalling packages"
-  yarn config set registry https://mirrors.zju.edu.cn/npm
-  yarn config set sharp_binary_host https://npmmirror.com/mirrors/sharp
-  yarn config set sharp_libvips_binary_host https://npmmirror.com/mirrors/sharp-libvips
+  yarn config set registry https://registry.npmmirror.com
+  yarn config set sharp_binary_host https://registry.npmmirror.com/-/binary/sharp
+  yarn config set sharp_libvips_binary_host https://registry.npmmirror.com/-/binary/sharp-libvips
   yarn install --frozen-lockfile
   sed -i 's/fonts.googleapis.com/fonts.loli.net/g' node_modules/gatsby-plugin-webfonts/modules/google.js
   cp yarn.lock node_modules/yarn.lock
